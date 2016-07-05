d2dec2196cc71eca709391fe62f957b2745392da
Tommy <TommyCohn116@gmail.com>
2016-07-05 15:02:56 -0400

    Cleared up the file structure in here.'

--- Webpage/script.js
+++ Webpage/script.js
@@ -1,7 +1,7 @@
 var webSocketIP = "127.0.0.1"; //The IP of the websocket server.
 var webSocketPort = "12345"; //The port the websocket is being served on.
 var webSocketPath = "/"; //The file path of the websocket.
-var formattedDataStringStandardArrayLength = 46; //The expected length of the formatted data string from formatRawMessage().
+var formattedDataStringExpectedArrayLength = 46; //The expected length of the formatted data string from formatRawMessage().
 var formattedDataStringPositionIndeces = [10, 11, 12]; //The location of the xyz position data in the formatted data string.
 var formattedDataStringQuaternionIndeces = [14, 15, 16, 17]; //The location of the xyzw quaternion data in the formatted data string.
 var eulerAngleUsed = 0; //Due to some weirdness with the robot's orientation data, the Euler angle for the XY-plane can be unexpected.
@@ -18,15 +18,19 @@ var pathMaxLength = Infinity; //If the program ever starts to get slow, this can
 						 	  //I'll set it to something once I find that point.
 var autoZoom = false; //This can be toggled. If it's true, the map will scale automatically so everything is visible.
 var scanRecord = []; //This is the list of laser scans. The indeces correspond with pointsRecord[]. //They're in x-y position format, the same as pointsRecord.
+var angleMinIndex = 37; //This is the formatted array index of the minimum angle for the scan.
+var angleMaxIndex = 38; //Same, but for the maximum angle.
+var rangeListIndex = 44; //Same, but for the list of ranges.
+var saveThisScan = false; //If true, the next scan will be saved.
 
 var canvas, context, dataArea, updateZoomButton, enterZoomTextArea, enterZoomButton, autoZoomButton, startButton; //These are global variables used for UI stuff.
 
-function setup() { //Call this to get the program going.
-	canvas = document.getElementById("mainCanvas"); //Grab the HTMl of the canvas.
+function setup() { //Call this to get the program going
+	canvas = document.getElementById("mainCanvas"); //Grab the HTML canvas element.
 	canvas.style.transform = "matrix(0, -1, 1, 0, 0, 0)"; //Rotate the canvas so up is forward, like in a map.
 	context = canvas.getContext("2d"); //All canvas drawings are done through a context.
 	context.fillStyle = "white"; //Set the fill style of closed shapes on the canvas to white.
-	context.beginPath(); //This starts a path so lines can be drawn.
+	context.beginPath(); //This starts a path with which lines can be drawn.
 
 	dataArea = document.getElementById("dataPrintout"); //As the program receives data, this area on the webpage can be used to record it.
 
@@ -41,109 +45,98 @@ function setup() { //Call this to get the program going.
 	}
 }
 function mainLoop(data) {
-	formatted = formatRawMessage(data); //This takes the raw data sent through the websocket, and converts it into something that's a bit easier to use.
-	console.log(formatted);
+	formattedMessage = formatRawMessage(data); //This takes the raw data sent through the websocket, and converts it into something that's a bit easier to use.
+	//console.log(formatted);
 
-	if(formatted.length == formattedDataStringStandardArrayLength) { //The formatted data should be an array with 46 units. If the array is a different length, something is wrong.
-		
-		//Position Data**************************************************
+	if(formattedMessage.length == formattedDataStringExpectedArrayLength) {
+		var positionXYZ = getPositionFromFormattedMessage(formattedMessage);
+		var quaternion = getQuaternionFromFormattedMessage(formattedMessage);
+		var eulerAngles = quaternionToEuler(quaternion);
+		var theta = eulerAngles[eulerAngleUsed];
+		var position = [positionXYZ[0], positionXYZ[1]]
 
-		//Store the x, y, and z position in a separate variable.
-		var positionXYZ = [formatted[formattedDataStringPositionIndeces[0]], formatted[formattedDataStringPositionIndeces[1]], formatted[formattedDataStringPositionIndeces[2]]];
+		pointsRecord.push(position);
 
-		//Store the x, y, z, and w quaternion in a separate variable.
-		var quaternionXYZW = [formatted[formattedDataStringQuaternionIndeces[1]], formatted[formattedDataStringQuaternionIndeces[0]], formatted[formattedDataStringQuaternionIndeces[2]], formatted[formattedDataStringQuaternionIndeces[3]]];
+		var angleMin = formattedMessage[angleMinIndex];
+		var angleMax = formattedMessage[angleMaxIndex];
+		var rangeList = formattedMessage[rangeListIndex];
+		var angleIncrement; //This is calculated, not read.
 
-		//Unfortunately, there's still more formatting to be done.
-		//The data entries stored in positionXYZ and quaternionXYZW are stored as strings, and have a bit before the number (e.g. "x:####").
-		//The "x:" part is removed by slicing from the character in position 2, and then the Number() function is called to convert the string to a useable number.
-
-		for(var i=0; i<positionXYZ.length; ++i) { //Format positionXYZ.
-			positionXYZ[i] = Number(positionXYZ[i].slice(2));
-		}
-		for(var i=0; i<quaternionXYZW.length; ++i) { //Format quaternionXYZW.
-			quaternionXYZW[i] = Number(quaternionXYZW[i].slice(2));
-		}
-
-		var eulerAngles = quaternionToEuler(quaternionXYZW); //Convert the quaternion to euler angles.
-		var theta = eulerAngles[eulerAngleUsed]; //This is the XY-plane angle actually used, rotated 90 degrees so that forward is up instead of right.
-
-		pointsRecord.push([positionXYZ[0], positionXYZ[1]]); //Store the next point to the list.
-
-		//Scan Data**************************************************
-
-		//Store the minimum and maximum angle ranges, and the scan data.
-		var angleMin = formatted[37];
-		var angleMax = formatted[38];
-		var rangeList = formatted[44];
-		var angleIncrement; //Calculate this later.
-
-		//Get rid of the text in front of angles.
-		angleMin = Number(angleMin.slice(10));
-		angleMax = Number(angleMax.slice(10));
-		
-		//Convert the string of an array of numbers into an array of numbers.
-		rangeList = rangeList.split(","); //Split along commas.
-		rangeList[0] = rangeList[0].slice(8); //Remove the characters at the beginning.
-		rangeList[rangeList.length-1] = rangeList[rangeList.length-1].slice(0, -1); //Get rid of that one last character.
-		for(var i=0; i<rangeList.length; ++i) {
-			rangeList[i] = Number(rangeList[i]); //Make them all numbers!
-		}
-
+		//Format all of the scan data.
+		var scanDataFormatted = cleanUpScanData(angleMin, angleMax, rangeList);
+		angleMin = scanDataFormatted[0];
+		angleMax = scanDataFormatted[1];
+		rangeList = scanDataFormatted[2];
 		angleIncrement = (angleMax - angleMin) / rangeList.length;
 
-		//console.log(angleMin);
-		//console.log(angleMax);
-		//console.log(rangeList);
+		var xyRangeList = convertRangeListToXY(angleMin, angleMax, angleIncrement, rangeList, position, theta);
 
-		var xyRobotRangeList = [];
-
-		for(var i=0; i<rangeList.length; ++i) {
-			var scanTheta = angleMin + (i * angleIncrement); //Calculate the angle that the current range value is using.
-
-			//Convert it to x, y form relative to the robot.
-			xyRobotRangeList[i] = [0, 0];
-			xyRobotRangeList[i][0] = rangeList[i] * Math.cos(scanTheta); //x=rcos(θ)
-			xyRobotRangeList[i][1] = rangeList[i] * -Math.sin(scanTheta); //y=rsin(θ)
+		if(saveThisScan || scanRecord.length == 0) {
+			scanRecord.push(xyRangeList);
+			saveThisScan = false;
 		}
-
-		scanRecord.push(xyRobotRangeList);
-
-		//Display**************************************************
+		else {
+			scanRecord[scanRecord.length - 1] = xyRangeList;
+		}
 
 		if(autoZoom) {
 			updateZoom(); //If autozoom is selected, automatically set the zoom.
 		}
 
-		context.lineWidth = 1/scaleFactor; //Make sure the lines don't freak out.
+		context.lineWidth = 1/scaleFactor;
 
 		clearCanvas();
+		setConstantCanvasTransforms();
 		drawRobotMarker();
-		drawCurrentMap(positionXYZ, xyRobotRangeList);
-		drawRobotPath(positionXYZ, theta);
+		setCanvasTransforms(position, theta);
+		drawRobotPath();
+		drawRobotMap();
+	}
+	else {
+		console.log("Error! The message is of an unexpected format!");
+		console.log(formattedMessage);
+	}
 
-		window.setTimeout(sendDataRequest, 100); //When using recorded data, use window.setTimeout().
-		//requestAnimationFrame(sendDataRequest); //When using data directly from the robot, use requestAnimationFrame().
-		//If you use requestAnimationFrame(), comment out EVERY debug statement, or the page will hang pretty quickly.
-	}
-	else { //Ok, so there's a problem with the data...
-		console.log("Improper data received!"); //Tell me wtf is going on.
-		console.log(data); //Print if out.
-		requestAnimationFrame(sendDataRequest); //Try again, to see if it gets better.
-	}
+	window.setTimeout(sendDataRequest, 100);
 }
 
-function sendDataRequest() {
-	ws.send("ready");
-	//When this message is sent, the server knows that the webpage is ready to process more data.
-	//The server will then proceed to send the most recent data avaiable.
-}
 function formatRawMessage(raw) { //This takes the raw message and formats it in a way that the data can be accessed more easily.
 	var refined = raw.split("\n"); //The data sent by the websocket has lots of newline characters -- every single piece of data is on its own line. This splits it apart into an array.
 	for(var i=0; i<refined.length; ++i) { //Now, for each item that was split apart...
 		refined[i] = refined[i].replace(/\s/g, ""); //Remove all the whitespace by replacing spaces with (quite literally) nothing.
 	}
 	return refined; //Spit it back at me.
+}
+function getPositionFromFormattedMessage(formattedMessage) {
+	var position = [0, 0, 0]; //[x, y, z]
+
+	//These are the locations of the three relavent data values.
+	position[0] = formattedMessage[formattedDataStringPositionIndeces[0]];
+	position[1] = formattedMessage[formattedDataStringPositionIndeces[1]];
+	position[2] = formattedMessage[formattedDataStringPositionIndeces[2]];
+
+	//This removes the stuff around the number itself in the string, and actually converts it to a number.
+	for(var i=0; i<position.length; ++i) {
+		position[i] = Number(position[i].slice(2));
+	}
+
+	return position;
+}
+function getQuaternionFromFormattedMessage(formattedMessage) {
+	var quaternion = [0, 0, 0, 0]; //[x, y, z, w]
+
+	//These are the locations of the four relavent data values.
+	quaternion[0] = formattedMessage[formattedDataStringQuaternionIndeces[0]];
+	quaternion[1] = formattedMessage[formattedDataStringQuaternionIndeces[1]];
+	quaternion[2] = formattedMessage[formattedDataStringQuaternionIndeces[2]];
+	quaternion[3] = formattedMessage[formattedDataStringQuaternionIndeces[3]];
+	
+	//This removes the stuff around the number itself in the string, and actually converts it to a number.
+	for(var i=0; i<quaternion.length; ++i) {
+		quaternion[i] = Number(quaternion[i].slice(2));
+	}
+
+	return quaternion;
 }
 function quaternionToEuler(quat) { //This takes the quaternion array [x, y, z, w] and returns the euler array [φ, θ, ψ]
 	//The quaternion describes the orientation and rotation of the robot, but it's very complicated.
@@ -156,6 +149,50 @@ function quaternionToEuler(quat) { //This takes the quaternion array [x, y, z, w
 	euler[1] = Math.asin(2*((quat[0]*quat[2]) - (quat[3]*quat[1])));
 	euler[2] = Math.atan2(2*((quat[0]*quat[3]) + (quat[1]*quat[2])), 1-(2*((quat[2]*quat[2]) + (quat[3]*quat[3]))));
 	return euler;
+}
+function cleanUpScanData(min, max, ranges) {
+	var angleMin = Number(min.slice(10));
+	var angleMax = Number(max.slice(10));
+
+	var rangeList = ranges.split(","); //Split along commas.
+	rangeList[0] = rangeList[0].slice(8); //Remove the characters at the beginning.
+	rangeList[rangeList.length - 1] = rangeList[rangeList.length - 1].slice(0, -1); //Get rid of that one last character.
+	for(var i=0; i<rangeList.length; ++i) {
+		rangeList[i] = Number(rangeList[i]); //Make them all numbers!
+	}
+
+	return [angleMin, angleMax, rangeList];
+}
+function convertRangeListToXY(min, max, increment, ranges, currentPosition, theta) {
+	var scanList = [];
+	var currentScan = [];
+	var scanTheta, x, y, x1, y1;
+	for(var i=0; i<ranges.length; ++i) {
+		scanTheta = min + (i * increment);
+
+		//Convert it from polar to cartesian.
+		currentScan[0] = ranges[i] * Math.cos(scanTheta);
+		currentScan[1] = ranges[i] * -Math.sin(scanTheta);
+
+		//Rotate it so that it's relative to the origin.
+		x = currentScan[0];
+		y = currentScan[1];
+		x1 = (x * Math.cos(-theta)) + (y * Math.sin(-theta));
+		y1 = (-1 * x * Math.sin(-theta)) + (y * Math.cos(-theta));
+		currentScan[0] = x1;
+		currentScan[1] = y1;
+
+		//Translate it so that it's relative to the origin.
+		currentScan[0] += currentPosition[0];
+		currentScan[1] += currentPosition[1];
+
+		var dataToPush = [currentScan[0], currentScan[1]];
+
+		//Push it into the scanList array.
+		scanList.push(dataToPush);
+	}
+
+	return scanList;
 }
 function updateZoom() {
 	//This function iterates through every point, determining which point is the furthest away from the current location.
@@ -190,6 +227,16 @@ function toggleAutoZoom() {
 	//This is called when you click the button to toggle automatic zoom.
 	autoZoom = !autoZoom;
 }
+function clearCanvas() {
+	context.setTransform(1, 0, 0, 1, 0, 0); //Reset all transforms on the context.
+	context.clearRect(0, 0, canvas.width, canvas.height); //Clear the canvas.
+	context.beginPath();
+}
+function setConstantCanvasTransforms() {
+	context.transform(1, 0, 0, 1, canvas.width/2, canvas.height/2); //Put 0, 0 in the center.
+	context.transform(1, 0, 0, -1, 0, 0); //Flip the canvas so y+ is up.
+	context.transform(scaleFactor, 0, 0, scaleFactor, 0, 0); //Scale the canvas.
+}
 function drawRobotMarker() {
 	//This will draw a circle around the center for the robot marker.
 	context.beginPath();
@@ -208,35 +255,42 @@ function drawRobotMarker() {
 	context.lineTo(robotMarkerRadius*Math.cos(Math.PI-robotMarkerArrowAngle), -robotMarkerRadius*Math.sin(Math.PI-robotMarkerArrowAngle));
 	context.stroke();
 }
-function clearCanvas() {
-	context.setTransform(1, 0, 0, 1, 0, 0); //Reset all transforms on the context.
-	context.clearRect(0, 0, canvas.width, canvas.height); //Clear the canvas.
-	context.transform(1, 0, 0, 1, canvas.width/2, canvas.height/2); //Put 0, 0 in the center of the canvas.
-	context.transform(scaleFactor, 0, 0, scaleFactor, 0, 0); //Scale the canvas.
-	context.transform(1, 0, 0, -1, 0, 0); //Flip the canvas so y+ is up.
+function setCanvasTransforms(position, theta) {
+	context.transform(Math.cos(-theta), Math.sin(-theta), -Math.sin(-theta), Math.cos(-theta), 0, 0); //Rotate the context so the path is properly oriented.
+	context.transform(1, 0, 0, 1, -position[0], -position[1]);
 }
-function drawRobotPath(currentPosition, angle) {
-	context.transform(Math.cos(-angle), Math.sin(-angle), -Math.sin(-angle), Math.cos(-angle), 0, 0); //Orient the path behind the robot properly.
-	context.moveTo(pointsRecord[0][0]-currentPosition[0], pointsRecord[0][1]-currentPosition[1]); //Move to the first point in the path.
-	context.beginPath();
-	for(var i=1; i<pointsRecord.length; ++i) { //This draws lines from point i to point i-1
-		context.lineTo(pointsRecord[i][0]-currentPosition[0], pointsRecord[i][1]-currentPosition[1]); //Draw a line to the next point.
+function drawRobotPath() {
+	context.moveTo(pointsRecord[0][0], pointsRecord[0][1]);
+	for(var i=1; i<pointsRecord.length; ++i) {
+		context.lineTo(pointsRecord[i][0], pointsRecord[i][1]);
 		context.stroke();
 	}
 }
-function drawCurrentMap(currentPosition, scan) {
-	context.moveTo(scan[0][0], scan[0][1]);
-	context.beginPath();
-	for(var i=1; i<scan.length; ++i) {
-		if(isNaN(scan[i][0]) || isNaN(scan[i][1]) || distance([scan[i][0], scan[i][1]], [scan[i-1][0], scan[i-1][1]]) > distanceDisplayThreshold) {
-			context.moveTo(scan[i][0], scan[i][1]);
-			context.beginPath();
-		}
-		else {
-			context.lineTo(scan[i][0], scan[i][1]);
-			context.stroke();
+function drawRobotMap() {
+	for(var i=0; i<scanRecord.length; ++i) {
+		scan = scanRecord[i];
+		context.moveTo(scan[0][0], scan[0][1]);
+		context.beginPath();
+		for(var j=1; j<scan.length; ++j) {
+			if(isNaN(scan[j][0]) || isNaN(scan[j][1]) || distance([scan[j][0], scan[j][1]], [scan[j-1][0], scan[j-1][1]]) > distanceDisplayThreshold) {
+				context.moveTo(scan[j][0], scan[j][1]);
+				context.beginPath();
+			}
+			else {
+				context.lineTo(scan[j][0], scan[j][1]);
+				context.stroke();
+			}
 		}
 	}
+}
+function sendDataRequest() {
+	ws.send("ready");
+	//When this message is sent, the server knows that the webpage is ready to process more data.
+	//The server will then proceed to send the most recent data avaiable.
+}
+function saveScan() {
+	saveThisScan = true;
+	//This comment is just here so the function will collapse in sublime.
 }
 
 //This actually sets it up so if you click "setup", the program starts.
@@ -251,3 +305,4 @@ enterZoomButton = document.getElementById("enterZoom");
 enterZoomButton.addEventListener("click", enterZoom);
 autoZoomButton = document.getElementById("autoZoom");
 autoZoomButton.addEventListener("click", toggleAutoZoom);
+document.getElementById("saveScan").addEventListener("click", saveScan);
